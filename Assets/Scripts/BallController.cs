b9845585c0c8dc6aa18fed8995c9de6b5892794d
sa-po1238 <60276072.kikuchi.cgp@gmail.com>
2023-05-23 13:21:30 +0900

    団子生成
    3種類の団子がランダムな箇所で降ってくるようにしました

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public GameObject dango;
+    public GameObject dangoG;
+    public GameObject dangoR;
+    public GameObject dangoW;
     public float fallSpeed = 5f;
-    public float interval = 1f;
+    public float intervalG = 1f;
+    public float intervalR = 2f;
+    public float intervalW = 3f;
     private float timer;
 
     // Start is called before the first frame update
@@ -22,15 +26,49 @@ public class BallController : MonoBehaviour
         timer += Time.deltaTime;
 
         // タイマーがインターバルを超えたら，画面上部からランダムにボールを生成
-        if (timer >= interval)
+        if (timer >= intervalG)
         {
-            timer = 0f;
 
-            Vector3 position = new Vector3(Random.Range(-9f, 9f), 5f, 0f);
-            GameObject ball = Instantiate(dango, position, Quaternion.identity);
+            //dangoG
+            Vector3 positionG = new Vector3(Random.Range(-9f, 9f), 5f, 0f);
+            GameObject ballG = Instantiate(dangoG, positionG, Quaternion.identity);
 
-            Rigidbody2D ballRigidbody = ball.GetComponent<Rigidbody2D>();
-            ballRigidbody.velocity = new Vector2(0f, -fallSpeed);
+            Rigidbody2D ballGRigidbody = ballG.GetComponent<Rigidbody2D>();
+
+            ballGRigidbody.velocity = new Vector2(0f, -fallSpeed);
+
+            //インターバルの更新
+            intervalG += 3f;
         }
+
+        if (timer >= intervalR)
+        {
+            //dangoR
+            Vector3 positionR = new Vector3(Random.Range(-9f, 9f), 5f, 0f);
+            GameObject ballR = Instantiate(dangoR, positionR, Quaternion.identity);
+
+            Rigidbody2D ballRRigidbody = ballR.GetComponent<Rigidbody2D>();
+
+            ballRRigidbody.velocity = new Vector2(0f, -fallSpeed);
+
+            //インターバルの更新
+            intervalR += 3f;
+        }
+
+        if (timer >= intervalW)
+        {
+
+            //dangoW
+            Vector3 positionW = new Vector3(Random.Range(-9f, 9f), 5f, 0f);
+            GameObject ballW = Instantiate(dangoW, positionW, Quaternion.identity);
+
+            Rigidbody2D ballWRigidbody = ballW.GetComponent<Rigidbody2D>();
+
+            ballWRigidbody.velocity = new Vector2(0f, -fallSpeed);
+
+            //インターバルの更新
+            intervalW += 3f;
+        }
+
     }
 }
