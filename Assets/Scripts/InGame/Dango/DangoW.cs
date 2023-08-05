03fe70fa91b679af355db0fb693139201207665a
Koga_hikaru <90139340+hikaru0331@users.noreply.github.com>
2023-08-06 01:26:29 +0900

    [add]ダンゴ９種類分のスコアデータを実装
    インターフェースを使った実装でダンゴ９種類分のスコアデータを実装

---
+++ Assets/Scripts/InGame/Dango/DangoW.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangoW : MonoBehaviour, IDangoInfo
+{
+    //public string Name { get; } = "";
+    //public Sprite Sprite { get; } = null;
+    //public string Color { get; } = "";
+    //public string Attribute { get; } = "";
+
+    public int Point { get; set; } = 10;
+    //public float Probability { get; set; } = 0.0f;
+}
