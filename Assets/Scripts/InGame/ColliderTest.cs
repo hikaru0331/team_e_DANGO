7ce93c2d9d661b27490093382242d37c0590ace0
Koga_hikaru <90139340+hikaru0331@users.noreply.github.com>
2023-08-06 01:50:34 +0900

    [fix]ゴールへのダンゴ画像表示のリファクタリング

--- Assets/Scripts/InGame/ColliderTest.cs
+++ Assets/Scripts/InGame/ColliderTest.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ColliderTest : MonoBehaviour
 {
-    int score;
+    private int score;
+    public Sprite dangoSprite;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,8 @@ public class ColliderTest : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //インターフェースとして用意したクラスのインスタンス化
+        //ダンゴに直接アタッチされていないが、アタッチされているクラス(DangoGなど)の親クラスのため取得可能
         IDangoInfo dangoInfo = collision.gameObject.GetComponent<IDangoInfo>();
 
         if (dangoInfo != null)
@@ -28,6 +32,15 @@ public class ColliderTest : MonoBehaviour
             Debug.Log(score); 
             Destroy(collision.gameObject);
         }
+
+        //ゴールにダンゴが刺される処理
+        //触れてきたオブジェクトのSpriteの情報を取得→ゴールのSpriteに代入
+        //ここでは簡易化のために串のSpriteそのものをダンゴに変更している
+        SpriteRenderer spriteRenderer = collision.gameObject.GetComponent<SpriteRenderer>();
+        dangoSprite = spriteRenderer.sprite;
+        
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = dangoSprite;
+
     }
 
 }
