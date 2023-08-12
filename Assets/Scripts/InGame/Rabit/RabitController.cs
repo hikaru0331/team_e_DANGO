cf14ebdb5276f0d84f90cff4501e55fc30b65e4c
Koga_hikaru <90139340+hikaru0331@users.noreply.github.com>
2023-08-13 01:07:25 +0900

    [add]ステージ完成

--- Assets/Scripts/InGame/Rabit/RabitController.cs
+++ Assets/Scripts/InGame/Rabit/RabitController.cs
@@ -10,11 +10,11 @@ public class RabitController : MonoBehaviour
  
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 4.0f)
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 5.0f)
         {
             rabit.transform.position += Vector3.right * moveSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -3.5f)
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -4.0f)
         {
             rabit.transform.position += Vector3.left * moveSpeed * Time.deltaTime;
         }
