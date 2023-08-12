393ddb80ece72934b098301776784c71fca7f695
hikaru0331 <90139340+hikaru0331@users.noreply.github.com>
2023-08-12 16:15:44 +0900

    [fix]Filterクラスにコメントアウトを追加

--- Assets/Scripts/InGame/StageObject/Filter/Filter.cs
+++ Assets/Scripts/InGame/StageObject/Filter/Filter.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+//Filter３種の親クラス
 public class Filter : MonoBehaviour
 {
+    //親オブジェクトのTransformを入れる変数
     private Transform parentTransform;
 
     // Start is called before the first frame update
     void Start()
     {
+        //親オブジェクトのTransformを取得
         parentTransform = transform.parent.GetComponent<Transform>();
 
+        //親オブジェクトの移動を制御
         parentTransform.DOLocalMoveX(3.5f, 3.0f)
            .SetEase(Ease.InOutQuad)
            .SetLoops(-1, LoopType.Yoyo);
     }
 
+    //Filter３種に入れる抽象メソッド
+    //ダンゴの色を変えるためのメソッド
     protected virtual void ChangeDangoColor()
     {
-
+        Debug.Log("オーバーライドが実行されていません");
     }
 
 }
