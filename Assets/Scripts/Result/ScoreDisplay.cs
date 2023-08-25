165b7968838a466e81390c09daf3e04a36a223ec
sa-po1238 <60276072.kikuchi.cgp@gmail.com>
2023-08-26 01:26:44 +0900

    [add]DoTweenがんばってる

--- Assets/Scripts/Result/ScoreDisplay.cs
+++ Assets/Scripts/Result/ScoreDisplay.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Threading.Tasks;
+using DG.Tweening;
 
 public class ScoreDisplay : MonoBehaviour
 {
     
     public GameObject scoreText;
+    public float scaleDuration = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,5 +30,6 @@ public class ScoreDisplay : MonoBehaviour
         
         TextMeshProUGUI scoreText = this.scoreText.GetComponent<TextMeshProUGUI>();
         scoreText.text = "スコア: " + Score.ToString();
+        //scoreText.transform.DOMove(0, 150, 0, scaleDuration);
     }
 }
