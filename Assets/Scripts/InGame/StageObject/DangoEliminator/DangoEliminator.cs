959e16643a5cf399ee42901160889186aa3c884e
hikaru0331 <90139340+hikaru0331@users.noreply.github.com>
2023-08-31 22:01:24 +0900

    [add]DangoEliminatorにタヌキランダム生成の処理を実装

--- Assets/Scripts/InGame/StageObject/DangoEliminator/DangoEliminator.cs
+++ Assets/Scripts/InGame/StageObject/DangoEliminator/DangoEliminator.cs
@@ -8,6 +8,13 @@ public class DangoEliminator : MonoBehaviour
     //DangoEliminatorのTransformを入れる変数
     private Transform eliminatorTransform;
 
+    //子オブジェクトのゴミ箱とタヌキを参照するための変数
+    [SerializeField] private GameObject dustBox;
+    [SerializeField] private GameObject raccoon;
+
+    private float timer;
+    private float raccoonInterval;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,9 +31,28 @@ public class DangoEliminator : MonoBehaviour
            .SetLoops(-1, LoopType.Yoyo);
     }
 
+    private void RaccoonSetter()
+    {
+        dustBox.gameObject.SetActive(false);
+        raccoon.gameObject.SetActive(true);
+    }
+
+    public void DustBoxSetter()
+    {
+        raccoon.gameObject.SetActive(false);
+        dustBox.gameObject.SetActive(true);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+
+        timer += Time.deltaTime;
+        raccoonInterval = Random.Range(45.0f, 90.0f);
+
+        if (timer >= raccoonInterval)
+        {
+            RaccoonSetter();
+        }
     }
 }
--- Assets/Scripts/InGame/StageObject/DangoEliminator/DustBox.cs
+++ Assets/Scripts/InGame/StageObject/DangoEliminator/DustBox.cs
@@ -3,13 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DustBox : MonoBehaviour
-{
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
+{  
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
