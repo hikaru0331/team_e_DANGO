5a93579db7b4f4ae4a7cdadd56711f138488fbc5
hikaru0331 <90139340+hikaru0331@users.noreply.github.com>
2023-09-02 02:17:14 +0900

    [fix]スコアを100単位に変更

--- Assets/Scripts/InGame/Dango/DangoGRabit.cs
+++ Assets/Scripts/InGame/Dango/DangoGRabit.cs
@@ -17,5 +17,5 @@ public class DangoGRabit : MonoBehaviour, IDangoInfo
     public string Color { get; set; } = "Green";
     public string Attribute { get; } = "Rabit";
 
-    public int Point { get; set; } = 50;
+    public int Point { get; set; } = 500;
 }
